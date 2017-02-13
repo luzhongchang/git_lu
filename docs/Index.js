ef1a9b83372960407bbd3f5df9171a24534d4b2c
git_lu <m15895632351@163.com>
2017-02-13 13:28:17 +0800

    #commit

--- docs/Index.js
+++ docs/Index.js
@@ -1,3 +1,17 @@
 /**
  * Created by luzhongchang on 17/1/30.
  */
+
+$(document).ready(function () {
+
+    $("img").click(function () {
+
+
+        $.get("demo_test.asp",function (data,status) {
+            alert('Data:' + data +'\nstatus' + status);
+        });
+        $("#productCategory").width(200).height(300);
+
+
+    })
+});
